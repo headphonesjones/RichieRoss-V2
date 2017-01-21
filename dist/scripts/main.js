8df71b2bf5c83f3605c87c3d5d57d4fb718d7f09
Jordan Johnson <josamjohn@gmail.com>
2017-01-20 17:33:22 -0700

    Bad commit, sorry

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -3,14 +3,45 @@
  */
 $(document).ready(function() {
         $("html").niceScroll();
+        // $(".strategyText").fadeOut(20);
     }
 );
 
-// $.stellar({
-//     hideDistantElements: false,
-//     responsive: true
-// });
+$.stellar({
+    hideDistantElements: false,
+    responsive: true,
+    horizontalScrolling: false
+});
 
+// $(".heroBlotch").stick_in_parent({offset_top: -600});
+
+$(".first").stick_in_parent()
+    .on("sticky_kit:stick", function(e) {
+        // $(".strategyText").fadeIn(2000);
+        $(".oppositeDots").fadeOut(1000);
+    })
+    .on("sticky_kit:unstick", function(e) {
+        // $(".strategyText").fadeOut(2000);
+        $(".oppositeDots").fadeIn(1000);
+    });
+
+$(".second").stick_in_parent({offset_top: -50})
+    .on("sticky_kit:stick", function(e) {
+        $(".coolGlasses").attr('src','/dist/images/cool-glasses.gif');
+    })
+    .on("sticky_kit:unstick", function(e) {
+        $(".coolGlasses").attr('src','/dist/images/first-frame.jpg');
+    });
+
+$window.scroll(function() {
+    var target = $('.strategyText');
+    var container = $('.first');
+    var targetHeight = target.outerHeight() + 250;
+    var scrollPercent = (window.pageYOffset) / targetHeight;
+    if (scrollPercent >= 0) {
+        target.css('opacity', Math.sin(scrollPercent * (Math.PI/2.0)));
+    }
+});
 // $(window).scroll(function() {
 //     var target = $('.heroBlotch');
 //     var container = $('.first');
@@ -23,35 +54,3 @@ $(document).ready(function() {
 //         target.css({"position": "fixed", "top": -targetHeight + 100 + "px"});
 //     }
 // });
-
-
-$.fn.moveIt = function(){
-    var $window = $(window);
-    var instances = [];
-
-    $(this).each(function(){
-        instances.push(new moveItItem($(this)));
-    });
-
-    window.onscroll = function(){
-        var scrollTop = $window.scrollTop();
-        instances.forEach(function(inst){
-            inst.update(scrollTop);
-        });
-    }
-}
-
-var moveItItem = function(el){
-    this.el = $(el);
-    this.speed = parseInt(this.el.attr('data-scroll-speed'));
-};
-
-moveItItem.prototype.update = function(scrollTop){
-    var pos = scrollTop / this.speed;
-    this.el.css('transform', 'translateY(' + -pos + 'px)');
-};
-
-// Initialization
-$(function(){
-    $('[data-scroll-speed]').moveIt();
-});
